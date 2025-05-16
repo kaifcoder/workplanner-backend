dd27550093db4df3f73c4ad048924fd7b8626018
meenu155 <meenusinghms392@gmail.com>
2025-05-16 23:22:43 +0530

    fix

--- src/main/java/com/example/workplanner/model/TaskStatus.java
+++ src/main/java/com/example/workplanner/model/TaskStatus.java
@@ -6,9 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
+
 public enum TaskStatus {
     PENDING,
     APPROVED,
